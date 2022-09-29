72570579ba6629b5b3c3eb11cad92ce36dda1934
Riley Seaburg <=>
2022-09-29 14:18:46 -0400

    initial commit

---
+++ lib/image.ex
@@ -0,0 +1,7 @@
+# Copyright (c) 2022 Evolving Software Corporation
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+defmodule Identicon.Image do
+  defstruct hex: nil, color: nil
+end
